06eecde4c39af05962c48f31dae76fa50002f730
moxiaogang <1106256891@qq.com>
2021-06-18 04:29:44 -0700

    add localad936x borad support

---
+++ localad936x.mk
@@ -0,0 +1,10 @@
+
+# Target specific constants go here
+
+#HDF_URL:=http://github.com/analogdevicesinc/plutosdr-fw/releases/download/${LATEST_TAG}/system_top.hdf
+TARGET_DTS_FILES:= zynq-localad936x.dtb
+COMPLETE_NAME:=LocalAd936x
+ZIP_ARCHIVE_PREFIX:=localad936x
+DEVICE_VID:=0x0456
+DEVICE_PID:=0xb673
+
